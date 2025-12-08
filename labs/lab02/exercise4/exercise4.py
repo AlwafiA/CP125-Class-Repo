b5376040bf87e71aad164b59a8c30c4bc3d95a2e
AlwafiA <alwafiabdulgader@gmail.com>
2025-12-08 13:05:55 +0800

    almost finish + howfix pytest problem?

--- labs/lab02/exercise4/exercise4.py
+++ labs/lab02/exercise4/exercise4.py
@@ -3,21 +3,22 @@
 
 def get_hourly_rate(vehicle_type, hour_24):
     if vehicle_type =="Electric":
-        rate = float(hour_24 * 2.00)
-    
+        rate = 2.00
+    elif vehicle_type == "Hybrid":
+        if hour_24 >= 6 and hour_24 <=22 : 
+            rate =  2.00
+        else :
+           rate =  5.00 
+    else :
+        rate = 5.00
+    return rate
+ 
 
-# Test your code here
-print("Testing Dynamic Parking Rate...")
-
-
-
-
-'''
-Situation:
-
-A parking system determines hourly rates dynamically:
-
-Electric vehicles pay RM2.00/hour at all times.
-Hybrid vehicles pay RM2.00/hour ONLY between 10pm (22) and 6am (6). Otherwise, they pay RM5.00/hour.
-All Other vehicles pay RM5.00/hour at all times.
-Task: Write a program that calculates and returns the correct hourly rate (float) based on vehicle type and hour.'''
+#Merge remote-tracking branch 'upstream/main'
+# Please enter a commit message to explain why this merge is necessary,
+# especially if it merges an updated upstream into a topic branch.
+#
+# Lines starting with '#' will be ignored, and an empty message aborts
+# the commit.
+#~                                                                                                                                                              ~                                                                                                                                                              ~                                                                                                                                                              ~                                                                                                                                                              ~                                                                                                                                                              .git/MERGE_MSG [unix] (07:59 01/01/1970)                                                                                                                6,1 All
+#E211: File ".git/MERGE_MSG" no longer available   
