248b67b350a1bd3877a824a2dae21cb74e18b8d4
AlwafiA <alwafiabdulgader@gmail.com>
2026-01-05 13:08:32 +0800

    restart vscode

--- labs/lab03/exercise1/exercise1.py
+++ labs/lab03/exercise1/exercise1.py
@@ -3,6 +3,7 @@ def count_bright_spots(pixels):
     for item in range(1, len(pixels)-1):
         if pixels[item] > pixels[item-1] and pixels[item] > pixels[item+1]:
             count += 1
+    print(count)
     return count
 
 pixels = [1,2,3,4,5,6]
