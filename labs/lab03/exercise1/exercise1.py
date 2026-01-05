017238ee2a90bf772cac61093ef01db7e7c0eb75
AlwafiA <alwafiabdulgader@gmail.com>
2026-01-05 13:07:06 +0800

    invalid syntax

--- labs/lab03/exercise1/exercise1.py
+++ labs/lab03/exercise1/exercise1.py
@@ -1,14 +1,9 @@
 def count_bright_spots(pixels):
     count = 0
-    for item in pixels:
-        if (item > item + 1)and (item > item-1):
+    for item in range(1, len(pixels)-1):
+        if pixels[item] > pixels[item-1] and pixels[item] > pixels[item+1]:
             count += 1
-        return count
-    """for item in pixels:
-        print(item)
+    return count
 
-pixel = ["ali" , "abu", "aka"]
-count_bright_spots(pixel)"""
-
-pixel = [50 , 100, 50]
-count_bright_spots(pixel)
+pixels = [1,2,3,4,5,6]
+count_bright_spots(pixels)
--- labs/lab03/exercise2/exercise2.py
+++ labs/lab03/exercise2/exercise2.py
@@ -1,5 +1,41 @@
-def find_station(stations, name):
-    pass
+"""def find_station(stations, name):
+    for item in range(len(stations)):
+        if stations[item] == name:
+            return item 
+    return -1
+
 
 def count_stops(stations, start, stop):
-    pass
+    count = 0
+    start_check = find_station(stations, start)
+    stop_check = find_station(stations, stop)
+    if start_check == -1 or stop_check == -1:
+        return -1
+    else :
+        for start in range(len(stations)):
+            if stations[start] != stop:
+                count +=1
+    print(count)
+    return count
+
+
+train = ["Central", "Marina", "Bukit", "Orchard", "Sentosa"]
+count_stops(train, "Marina", "Sentosa")"""
+
+def find_station(stations, name):
+    for i in range(len(stations)):
+        if stations[i] == name:
+            return i
+    return -1
+
+def count_stops(stations, start, stop):
+    start_idx = find_station(stations, start)
+    stop_idx = find_station(stations, stop)
+    
+    if start_idx == -1 or stop_idx == -1:
+        return -1
+        
+    return abs(start_idx - stop_idx)
+
+train = ["Central", "Marina", "Bukit", "Orchard", "Sentosa"]
+count_stops(train, "Marina", "Sentosa")
