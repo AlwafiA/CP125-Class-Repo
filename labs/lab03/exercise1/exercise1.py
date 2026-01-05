5f59a6821169d31e7e032d6ecb62ff61a840d5d8
AlwafiA <alwafiabdulgader@gmail.com>
2026-01-05 11:55:32 +0800

    error

--- labs/lab03/exercise1/exercise1.py
+++ labs/lab03/exercise1/exercise1.py
@@ -1,2 +1,14 @@
 def count_bright_spots(pixels):
-    pass
+    count = 0
+    for item in pixels:
+        if (item > item + 1)and (item > item-1):
+            count += 1
+        return count
+    """for item in pixels:
+        print(item)
+
+pixel = ["ali" , "abu", "aka"]
+count_bright_spots(pixel)"""
+
+pixel = [50 , 100, 50]
+count_bright_spots(pixel)
