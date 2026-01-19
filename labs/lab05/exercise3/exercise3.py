277d07025437dc2d16d021e313f92e0531820129
AlwafiA <alwafiabdulgader@gmail.com>
2026-01-19 13:11:52 +0800

    ex4

--- labs/lab05/exercise3/exercise3.py
+++ labs/lab05/exercise3/exercise3.py
@@ -1,12 +1,20 @@
 
 def find_bottleneck_index(traceroute):
-    """
-    Find the index of the hop where the largest latency jump begins.
-    """
-    pass
+    biggest_jump = 0
+    bottleneck_index = 0
+    for i in range(len(traceroute)-1):
+        x1, y1 = traceroute[i]
+        x2, y2 = traceroute[i + 1]
+        if  y2 > y1:
+            jump = abs(y2-y1)
+            if jump > biggest_jump:
+                biggest_jump = jump
+                bottleneck_index = i
+            
+    return bottleneck_index
 
 
 # Test
-traceroute = ((1, 5), (2, 8), (3, 45), (4, 48), (5, 50))
+traceroute = ((1, 50), (2, 50), (3, 50), (4, 50))
 result = find_bottleneck_index(traceroute)
 print(f"Bottleneck Index: {result}")  # Expected: 1
