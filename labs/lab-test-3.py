07b9af84ed96624295ea644a9b76949a6ffab5a7
AlwafiA <alwafiabdulgader@gmail.com>
2026-03-16 12:22:05 +0800

    done

---
+++ labs/lab-test-3.py
@@ -0,0 +1,43 @@
+#ALWAFI ABDULGADER ALAMIN
+#MC2515202741
+
+
+import csv 
+
+def initial_csv_calculate(bmi_csv):
+    file = open(bmi_csv, 'r' , newline="")
+    file_reader = csv.reader(file)
+    
+    total_height = 0
+    count = 0
+    for row in file_reader:
+        if row[1] == "Height":
+            print(row)
+        else : 
+            total_height += float(row[1])
+            count +=1
+            print(row)
+    
+    avg_height = total_height / count 
+    print(f"The Average is {avg_height}")
+    file.close()
+
+def insert_new_row(bmi_csv):
+    gender = input("Enter gender : ")
+    height = input("Enter height : ")
+    weight = input("Enter weight : ")
+    bmi_num = input("Enter bmi : ")
+
+    file2 = open(bmi_csv, 'a', newline='')
+    file2_writer = csv.writer(file2)
+    file2_writer.writerow([gender, height, weight, bmi_num])
+
+  
+    file2 = open(bmi_csv, 'r', newline='') 
+    for rows in csv.reader(file2):
+        print(rows)
+
+initial_csv_calculate("bmi.csv")
+insert_new_row("bmi.csv")
+
+
